d8fe39f4e6a4f55a139616c842975091d25d6932
daniele.corvari00@gmail.com <daniele.corvari00@gmail.com>
2025-05-08 20:37:57 +0200

    pke

--- include/pke.h
+++ include/pke.h
@@ -11,15 +11,33 @@
 
 using namespace std;
 using namespace Eigen;
-class PKE{
+class PKE {
     public:
         PKE();
-        ~PKE();
-        tuple<PolynomialMatrix<1, PARAM_D>,PolynomialMatrix<1, PARAM_D>,vector<uint8_t>> KeyGen(const vector<uint8_t>& seed);
-        pair<PolynomialMatrix<1, PARAM_D>,PolynomialMatrix<1, 1>> Encrypt(PolynomialMatrix<1, PARAM_D>t,const vector<uint8_t>& rho,const vector<uint8_t>& m, const vector<uint8_t>& r) ;
-        vector<uint8_t> Decrypt(PolynomialMatrix<1, PARAM_D> u, PolynomialMatrix<1, 1> v,PolynomialMatrix<1, PARAM_D> s);
-
-};
+        virtual ~PKE();  // distruttore virtuale, buona pratica
+    
+        // rendiamo virtual tutti i metodi che vogliamo ridefinire
+        virtual
+        tuple<PolynomialMatrix<1, PARAM_D>,
+              PolynomialMatrix<1, PARAM_D>,
+              vector<uint8_t>>
+        KeyGen(const vector<uint8_t>& seed);
+    
+        virtual
+        pair<PolynomialMatrix<1, PARAM_D>,
+             PolynomialMatrix<1, 1>>
+        Encrypt(PolynomialMatrix<1, PARAM_D> t,
+                const vector<uint8_t>& rho,
+                const vector<uint8_t>& m,
+                const vector<uint8_t>& r);
+    
+        virtual
+        vector<uint8_t>
+        Decrypt(PolynomialMatrix<1, PARAM_D> u,
+                PolynomialMatrix<1, 1> v,
+                PolynomialMatrix<1, PARAM_D> s);
+    };
+    
 
 
 #endif
